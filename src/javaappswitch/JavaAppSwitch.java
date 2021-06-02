08014bf22b4e459133d7d261a51016e459857c19
BorgePambo <borginhopambo01@gmail.com>
2021-06-02 08:02:11 -0300

    Primeiro Projeto

---
+++ src/javaappswitch/JavaAppSwitch.java
@@ -0,0 +1,46 @@
+package javaappswitch;
+
+import java.util.Scanner;
+
+public class JavaAppSwitch {
+
+   
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        String dia;
+        
+        switch(x){
+            case 1: 
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda";
+                break;
+            case 3:
+                dia = "Terça";
+                break;
+            case 4:
+                dia = "Quarta";
+                break;
+            case 5:
+                dia = "Quinta";
+                break;
+            case 6: 
+                dia = "Sexta";
+                break;
+            case 7:
+                dia = "Sábado";
+                break;
+            default:
+                dia = "Dia inválido";
+                                
+        }
+        
+        System.out.println("Dia de Semana é: " + dia);
+        
+    }
+    
+}
